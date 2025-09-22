c2c27a4fb3f56ed750b4e0d88759341388ce156d
Rakibul-98 <rakibul.rupom2001@gmail.com>
2025-09-22 10:51:53 +0600

    db is connected and verify the connection successfully

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,24 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+
+dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB connected successfully!");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
 
 app.get("/", (req, res) => {
-  res.send("Server is running!");
+  res.send("Server running & DB connected successfully!");
 });
 
 app.listen(PORT, () => {
