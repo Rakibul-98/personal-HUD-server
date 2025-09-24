6ea366800f166ff3f5eb2dc704a252b7723e2d5a
Rakibul-98 <rakibul.rupom2001@gmail.com>
2025-09-24 11:15:43 +0600

    fixing deployment error

--- dist/server.js
+++ dist/server.js
@@ -3,13 +3,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-const PORT = 5000;
-app.get("/", (req, res) => {
-    res.send("Server is running!");
-});
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-});
+const app_1 = __importDefault(require("./app"));
+const feedScheduler_1 = require("./modules/feed/fetchers/feedScheduler");
+const database_1 = require("./shared/config/database");
+const env_1 = require("./shared/config/env");
+const startServer = async () => {
+    try {
+        await (0, database_1.connectDatabase)();
+        (0, feedScheduler_1.startFeedScheduler)();
+        app_1.default.listen(env_1.env.port, () => {
+            console.log(`Server running on port ${env_1.env.port} in ${env_1.env.nodeEnv} mode`);
+        });
+    }
+    catch (error) {
+        console.error("Failed to start server:", error);
+        process.exit(1);
+    }
+};
+startServer();
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import app from "./app";
-import { startFeedScheduler } from "./modules/feed/fetchers/feedScheduler";
+// import { startFeedScheduler } from "./modules/feed/fetchers/feedScheduler";
 import { connectDatabase } from "./shared/config/database";
 import { env } from "./shared/config/env";
 
@@ -7,7 +7,7 @@ const startServer = async () => {
   try {
     await connectDatabase();
 
-    startFeedScheduler();
+    // startFeedScheduler();
 
     app.listen(env.port, () => {
       console.log(`Server running on port ${env.port} in ${env.nodeEnv} mode`);
