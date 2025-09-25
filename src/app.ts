740343c08c14f346fe581ea518c90c5e886fb611
Rakibul-98 <rakibul.rupom2001@gmail.com>
2025-09-25 23:56:24 +0600

    added lclient live link to cors

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,15 @@ import { Request, Response } from "express";
 
 const app: Application = express();
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://personal-hud-client.vercel.app/",
+    ],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 
