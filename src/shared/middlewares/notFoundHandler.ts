ef09c80a8638344960b1cd9338878089d7c1e25a
Rakibul-98 <rakibul.rupom2001@gmail.com>
2026-05-02 10:25:54 +0600

    fixed a development bug

--- src/shared/middlewares/notFoundHandler.ts
+++ src/shared/middlewares/notFoundHandler.ts
@@ -2,5 +2,5 @@ import { Request, Response } from "express";
 import { ApiResponse } from "../utils/ApiResponse";
 
 export const notFoundHandler = (req: Request, res: Response) => {
-  res.status(404).json(ApiResponse.error("Route not found"));
+  res.status(404).json(ApiResponse.fail("Route not found"));
 };
